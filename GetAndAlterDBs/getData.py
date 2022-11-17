23441ece5d0c9ce562141e7358f31deeab68e395
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-11-17 16:53:45 +0100

    rickAndMortyDataReady

--- GetAndAlterDBs/getData.py
+++ GetAndAlterDBs/getData.py
@@ -4,9 +4,40 @@ movieData = "../data/Movies.csv"
 
 df = pd.read_csv(movieData, usecols = ["movie_title", "genre", "studio_name", "directors"])
 
-df_new = df.rename(columns={"movie_title": "guessWord", "genre": "hint", "directors": "hint2", "studio_name": "hint3"})
+df_new = df.rename(columns={"movie_title": "guessWord", "genre": "genreHint", "directors": "directorsHint", "studio_name": "studioHint"})
 
-print(df_new)
+# print(df_new)
+with open("../data/MyMovieDB.csv", "w") as f:
+    resultsDF = pd.DataFrame.from_dict(df_new)
+    resultsDF_CSV = resultsDF.to_csv(index=False)
+    f.write(resultsDF_CSV)
+
 ####################################################################################################
 
-rickAndMortyUrl = "https://rickandmortyapi.com/"
+import requests
+import json
+
+response = requests.get("https://rickandmortyapi.com/api/character")
+        
+results = response.json()["results"]
+
+with open("../data/rickAndMortyDB.csv", "w") as f:
+    resultsDF = pd.DataFrame.from_dict(results)
+    resultsDF_CSV = resultsDF.to_csv(index=False)
+    f.write(resultsDF_CSV)
+
+df = pd.read_csv("../data/rickAndMortyDB.csv", usecols = ["name", "gender", "species", "origin"])
+
+def reformatOrigin(origin):
+    
+    test = origin.split("'")[3].strip()
+    
+    return test
+
+df['origin'] = df['origin'].apply(reformatOrigin)
+df_new = df.rename(columns={"name": "guessWord", "gender": "genderHint", "species": "speciesHint", "origin": "originHint"})
+
+with open("../data/MyRickAndMortyDB.csv", "w") as f:
+    resultsDF = pd.DataFrame.from_dict(df_new)
+    resultsDF_CSV = resultsDF.to_csv(index=False)
+    f.write(resultsDF_CSV)
