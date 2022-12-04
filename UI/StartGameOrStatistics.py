4dcba92d4fca1b7c37d9125bb6f12d83465a6f9a
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-12-04 13:23:12 +0100

    made the UI for start game or statistics

--- UI/StartGameOrStatistics.py
+++ UI/StartGameOrStatistics.py
@@ -4,7 +4,7 @@ import tkinter.font as font
 from tkinter import ttk
 from getLines import showLines
 
-def chooseACategoryPage(userName):
+def startOrStatistics(userName):
     root = Tk()
     root.title("Hangman")
     root.iconbitmap("HangmanIcon.ico")
@@ -37,8 +37,8 @@ def chooseACategoryPage(userName):
     headline.pack(ipadx=2)
 
     ##############################text################################
-    category = Label(contentFrame, text="Choose a category", bg=black, fg=white, font=("Arial", myFontSize))
-    category.grid(row=0, column=0, columnspan=3, sticky=W+N+E+S, pady=5)
+    whatToDo = Label(contentFrame, text="What do you want to do?", bg=black, fg=white, font=("Arial", myFontSize))
+    whatToDo.grid(row=0, column=0, columnspan=3, sticky=W+N+E+S, pady=5)
     
     ##############################button functions################################    
     def exitGame():
@@ -46,13 +46,13 @@ def chooseACategoryPage(userName):
         root.quit()      
 
     ##############################buttons################################
-    exitButton = Button(contentFrame, text="Exit", command=exitGame, width=8, fg=white, bg=red, font=("Arial", myFontSize))
-    exitButton.grid(row=3, column=0, sticky=W+N+S+E, ipadx=10, pady=25)
+    exitButton = Button(contentFrame, text="Exit", command=exitGame, width=15, fg=white, bg=red, font=("Arial", myFontSize))
+    exitButton.grid(row=3, column=0, sticky=W+N+S+E, ipadx=10, pady=25, padx=20)
     
-    movieButton = Button(contentFrame, text="Movie names", command=lambda: movies(), fg=white, bg=blue, font=("Arial", myFontSize))
-    movieButton.grid(row=1, column=0, sticky=W+N+S+E, ipadx=10, pady=25)
+    seeStatistics = Button(contentFrame, text="See statistics", command=lambda: movies(), width=15, fg=white, bg=blue, font=("Arial", myFontSize))
+    seeStatistics.grid(row=1, column=0, sticky=W+N+S+E, ipadx=10, pady=25, padx=20)
 
-    RAMButton = Button(contentFrame, text="Rick and morty characters", command=lambda: ram(), fg=white, bg=blue, font=("Arial", myFontSize))
-    RAMButton.grid(row=2, column=0, sticky=W+N+S+E, ipadx=10, pady=25)
+    PlayTheGame = Button(contentFrame, text="Play the game", command=lambda: ram(), width=15, fg=white, bg=blue, font=("Arial", myFontSize))
+    PlayTheGame.grid(row=2, column=0, sticky=W+N+S+E, ipadx=10, pady=25, padx=20)
     
     root.mainloop()
