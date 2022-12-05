33bc11ce0f767fb5f325f55bb8b483f9796485e1
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-12-05 12:38:16 +0100

    made the file that starts the game on the log in and sign up page

---
+++ go.py
@@ -0,0 +1,8 @@
+import sys
+# to get from a ../ folder 
+sys.path.append("..")
+
+# This is the file that will start the game on the log in and sign up page
+from UI.LogInSignUpPage import loginSignUpPage
+
+loginSignUpPage()
