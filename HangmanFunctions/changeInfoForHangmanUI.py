4718004c2d8955741589120f4802bc7b6542102a
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-12-05 12:35:44 +0100

    made a function that takes a random row, makes it readable and sends it to the hangman game page

---
+++ HangmanFunctions/changeInfoForHangmanUI.py
@@ -0,0 +1,24 @@
+from UI.HangmanGameUI import hangmanGamePage
+from UI.getLines import showLines
+import re
+
+def changeInfoForHangmanUI(user, toChange):
+    guessWord = str(toChange["guessWord"].values[0]).strip()
+
+    category = str(toChange["category"].values[0]).strip()
+
+    if category == "RAMC":
+        category = "Rick and Morty character"
+
+    sentenceInfo = str(toChange["sentenceInfo"].values[0]).strip()
+    lines = showLines(sentenceInfo)
+
+    hints = []
+    hintColumns = [col for col in toChange.columns if 'Hint' in col]
+
+    for i in hintColumns:
+        hintFix = str(toChange[i].values[0])
+        hints.append(hintFix)
+
+    hangmanGamePage(user, guessWord, category, lines, "", hints)
+
